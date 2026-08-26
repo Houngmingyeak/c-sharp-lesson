de2c46d68854d71b8e00ad5fb9d50e9d158cacbc
houngmingyeak <huangmingyeak372006@gmail.com>
2026-08-26 10:17:46 +0700

    c# first day

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace First_Day
+{
+    internal class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace First_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Ca = new Calculator();
+
+            Console.Write("Enter Value A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Value B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum= " + Ca.add(A, B));
+            Console.WriteLine("Mul= " + Ca.mul(A, B));
+            Console.WriteLine("Div= " + Ca.div(A, B));
+            Console.WriteLine("Sub= " + Ca.sub(A, B));
+
+            Console.ReadKey();
+        }
+    }
+}
